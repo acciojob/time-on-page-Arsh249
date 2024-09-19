6ad5a521e10cc55977cf79313c9eaedf21567ce5
Arsh Ilahi <arshilahi74@gmail.com>
2024-09-19 10:27:41 +0530

    s

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,24 @@
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    // Set up a timer that increments the seconds count every 1 second
+    const intervalId = setInterval(() => {
+      setSeconds((prevSeconds) => prevSeconds + 1);
+    }, 1000);
+
+    // Clean up the timer when the component is unmounted
+    return () => clearInterval(intervalId);
+  }, []);
+
+
   return (
     <div>
-        {/* Do not remove the main div */}
+       <h1>You have been on this page for {seconds} seconds.</h1>
     </div>
   )
 }
